c474514d289e84cd5eb621e0cde5ffc146144798
poketony <57011422+poketony@users.noreply.github.com>
2026-05-17 01:45:09 +0900

    챕터 5 플레이 검수 완료

---
+++ StreamingAssets/aa/StandaloneWindows64/file/WorkSpace/pp05/help0/RepackHelp0.bat
@@ -0,0 +1,2 @@
+python xhf_text_tool_offsets_full.py repack help0.xhf.ori help0.txt help0.xhf --charmap xenosaga_charmap.json --no-preserve-first-section-base --export-offsets help_offsets.json
+pause
